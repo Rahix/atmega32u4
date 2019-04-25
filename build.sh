160d4e8383158c075a5519ea06a1ef4ccdc0f935
Rahix <rahix@rahix.de>
2019-04-25 14:17:18 +0200

    Fix svd build script for svd2rust 0.14.0
    Fix #3
    
    Signed-off-by: Rahix <rahix@rahix.de>

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -xe
 
 ./gen-svd.py > _.svd
-svd2rust --target none -i _.svd >mod.rs
+svd2rust --target none -i _.svd
 rm _.svd
-rm -r src/peripherals
-form -i mod.rs -o src/peripherals 2>/dev/null
-rm mod.rs
+rm -rf src/peripherals
+form -i lib.rs -o src/peripherals 2>/dev/null
+rm lib.rs
 mv src/peripherals/lib.rs src/peripherals/mod.rs
 cargo fmt || true
 
@@ -15,9 +15,10 @@ sed -i '1d; 2d' src/peripherals/mod.rs
 
 # Patch DEVICE_PERIPHERALS
 sed -i 's/\(static mut DEVICE_PERIPHERALS\)/pub(crate) \0/' src/peripherals/mod.rs
-sed -i 's/\(interrupt::Interrupt\)/self::\0/' src/peripherals/mod.rs
 
 # Generate interrupt macro
+mkdir src/peripherals/interrupt
+mv src/peripherals/interrupt.rs src/peripherals/interrupt/mod.rs
 ./int_macro.sh >src/peripherals/interrupt/vector_macro.rs
 sed -i '1ipub mod vector_macro;' src/peripherals/interrupt/mod.rs
 
